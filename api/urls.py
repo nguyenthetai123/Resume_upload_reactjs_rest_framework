3075fd251114292e3b7a82d56df71dd392130dde
nguyenthetai123 <nguyenthetai.itdev97@gmail.com>
2023-12-28 21:58:35 +0700

    backend

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+  path('resume/', views.ProfileView.as_view(), name='resume'),
+  path('list/', views.ProfileView.as_view(), name='list')
